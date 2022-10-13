1ed2486cb4037fa28131d18522d5135b0f4df2bc
garyb-bs <gary.b@browserstack.com>
2022-10-13 13:44:16 +0100

    Initial commit

---
+++ cypress/e2e/spec.login.cy.js
@@ -0,0 +1,35 @@
+describe('Sky Lesson 1 Exercise', () => {
+  
+  it('should login successfully', () => {
+    cy.visit('https://practicetestautomation.com/practice-test-login');
+
+    cy.get('#username').type('student'); 
+    cy.get('#password').type('Password123');
+    cy.get('#submit').click();
+
+    cy.get('.post-title').should('be.visible');
+  })
+
+  it('should fail to login because of username', () => {
+    cy.visit('https://practicetestautomation.com/practice-test-login');
+
+    cy.get('#username').type('wrongstudent'); 
+    cy.get('#password').type('Password123');
+    cy.get('#submit').click();
+
+    cy.get('#error').should('be.visible');
+    cy.get('#error').invoke('text').should('match', /^Your username is invalid!/);
+  })
+
+  it('should fail to login because of password', () => {
+    cy.visit('https://practicetestautomation.com/practice-test-login');
+
+    cy.get('#username').type('student'); 
+    cy.get('#password').type('Password456');
+    cy.get('#submit').click();
+
+    cy.get('#error').should('be.visible');
+    cy.get('#error').invoke('text').should('match', /^Your password is invalid!/);
+  })
+  
+})
